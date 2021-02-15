aa84daad4fd0abbe918d81ede1cc51fb2b207ce9
davepetrov <davidpetrovx@gmail.com>
2021-02-15 11:47:06 -0500

    Setup foundational springboot config

---
+++ src/main/java/com/tickersocial/tickersocial/TickersocialApplication.java
@@ -0,0 +1,13 @@
+package com.tickersocial.tickersocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickersocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickersocialApplication.class, args);
+    }
+
+}
